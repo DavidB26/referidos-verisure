588a3db275138eee90f20ba046b4a16ebdb10f8f
David <luis.beslanga@verisure.pe>
2026-01-16 10:50:10 -0500

    Ajustes supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,4 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(url, anon);
+export const supabase = createClient(url, anon, {
+  auth: {
+    flowType: "pkce",
+    detectSessionInUrl: true,
+  },
+});
